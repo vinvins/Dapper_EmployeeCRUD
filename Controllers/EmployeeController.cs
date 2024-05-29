2c7730e3278bf8a4a78594101434a91ae7245001
vinvins <vinnarasivins@gmail.com>
2024-05-29 23:22:29 +0530

    Dapper Project CRUD Employee

---
+++ Controllers/EmployeeController.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using EmployeeTest.Models;
+using Dapper;
+
+namespace EmployeeTest.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        // GET: Employee
+        public ActionResult Index()
+        {
+            IEnumerable<Employee> employees= DapperORM.ReturnList<Employee>("GetAllEmployees");
+            return View(employees);
+        }
+
+        
+        public ActionResult Create(int EmployeeID=0)
+        {           
+            return View();
+        }
+
+      
+        [HttpPost]
+        public ActionResult Create(Employee employee)
+        {
+            DynamicParameters param = new DynamicParameters();
+            param.Add("@EmployeeID", employee.EmployeeID);
+            param.Add("@Name", employee.Name);
+            param.Add("@Age", employee.Age);
+            param.Add("@Salary", employee.Salary);
+            DapperORM.ReturnNothing("AddorEditEmployee",param);
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Details(int ID)
+        {
+            DynamicParameters param = new DynamicParameters();
+            param.Add("@EmployeeID", ID);
+            Employee employee = DapperORM.ReturnSingleRow<Employee>("GetEmployeeByID", param);
+            return View(employee);
+        }
+
+        public ActionResult Edit(int ID)
+        {
+            DynamicParameters param = new DynamicParameters();
+            param.Add("@EmployeeID", ID);
+            Employee employee = DapperORM.ReturnSingleRow<Employee>("GetEmployeeByID", param);
+            return View(employee);
+        }
+
+        [HttpPost]
+        public ActionResult Edit(Employee employee)
+        {
+            DynamicParameters param = new DynamicParameters();
+            param.Add("@EmployeeID", employee.EmployeeID);
+            param.Add("@Name", employee.Name);
+            param.Add("@Age", employee.Age);
+            param.Add("@Salary", employee.Salary);
+            DapperORM.ReturnNothing("AddorEditEmployee", param);
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Delete(int ID)
+        {
+            DynamicParameters param = new DynamicParameters();
+            param.Add("@EmployeeID", ID);
+            Employee employee = DapperORM.ReturnSingleRow<Employee>("GetEmployeeByID", param);
+            return View(employee);
+        }
+
+        [HttpPost]
+        public ActionResult Delete(Employee employee,int ID)
+        {
+            DynamicParameters param = new DynamicParameters();
+            param.Add("@EmployeeID", ID);
+            DapperORM.ReturnNothing("DeleteEmployeeByID", param);
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ Models/DapperORM.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Dapper;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace EmployeeTest.Models
+{
+    public static class DapperORM
+    {
+        public static string ConnectionString = "Data source=.;Initial catalog=EmployeeTest;Integrated security=True;";
+
+        public static IEnumerable<T> ReturnList<T>(string StoredProcedure, DynamicParameters param=null)
+        {
+            using(SqlConnection con=new SqlConnection(ConnectionString))
+            {
+                return con.Query<T>(StoredProcedure, param, commandType: CommandType.StoredProcedure);
+            }
+        }
+
+        public static void ReturnNothing(string StoredProcedure, DynamicParameters param = null)
+        {
+            using (SqlConnection con = new SqlConnection(ConnectionString))
+            {
+                con.Execute(StoredProcedure, param, commandType: CommandType.StoredProcedure);
+            }
+        }
+
+        public static T ReturnSingleRow<T>(string StoredProcedure, DynamicParameters param = null)
+        {
+            using (SqlConnection con = new SqlConnection(ConnectionString))
+            {
+                return (T) con.QuerySingle<T>(StoredProcedure, param, commandType: CommandType.StoredProcedure);
+            }
+        }
+    }
+}
